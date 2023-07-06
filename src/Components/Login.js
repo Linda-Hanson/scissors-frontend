d27a300a07ed0f7868cc73956f82fd3562ec7014
Linda Hanson <107373537+Linda-Hanson@users.noreply.github.com>
2023-07-06 13:49:28 +0100

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -22,7 +22,7 @@ function Login({ loginData, setLoginData }) {
         return;
       }
 
-      var url = `http://localhost:8000/api/users/login`;
+      var url = `https://scissors-api-ew6j.onrender.com/api/users/login`;
       fetch(url, {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
