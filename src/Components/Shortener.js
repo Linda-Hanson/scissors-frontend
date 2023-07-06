2bc45f408e4e93cb8af008fc1e1637baaa63cb43
Linda Hanson <107373537+Linda-Hanson@users.noreply.github.com>
2023-07-06 13:50:57 +0100

    Update Shortener.js

--- src/Components/Shortener.js
+++ src/Components/Shortener.js
@@ -19,7 +19,7 @@ function Shortener({ loginData, setLoginData, apiKey, setApiKey }) {
 
     let api_url =
     //   process.env.NODE_ENV === 'production'
-        `http://localhost:8000/api/urls/?user=${loginData.email}`
+        `https://scissors-api-ew6j.onrender.com/api/urls/?user=${loginData.email}`
         // : `http://localhost:5000/api/urls/?user=${loginData.email}`;
 
     console.log(
@@ -85,7 +85,7 @@ function Shortener({ loginData, setLoginData, apiKey, setApiKey }) {
       : `Bearer ${loginData.token}`;
 
     if (url.match(regex)) {
-      let api_url = `http://localhost:8000/api/urls/encrypt/`;
+      let api_url = `https://scissors-api-ew6j.onrender.com/api/urls/encrypt/`;
 
       let headers = {
         'Content-Type': 'application/json',
@@ -131,7 +131,7 @@ function Shortener({ loginData, setLoginData, apiKey, setApiKey }) {
       : `Bearer ${loginData.token}`;
 
     if (url.match(regex)) {
-      let api_url = `http://localhost:8000/api/urls/`;
+      let api_url = `https://scissors-api-ew6j.onrender.com/api/urls/`;
 
       let headers = {
         'Content-Type': 'application/json',
